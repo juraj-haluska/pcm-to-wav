662c31ad2f557c488ff960a829a2698c0977a0f4
Juraj Haluska <Juraj Haluska>
2017-01-22 18:24:08 +0200

    this is not good

--- main.c
+++ main.c
@@ -1,12 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "main.h"
 
-#define SAMPLE_RATE 44100
-
-#define DATA_SIZE   1024
-
+#define DATA_SIZE   44100   // 1 second
 
 
 int main() {
     printf("Hello, World!\n");
+
+    short * data = malloc(sizeof(short) * DATA_SIZE);
+
+    struct t_header header;
+    header.chunkId = RIFF;
+    header.chunkSize = sizeof(struct t_header) + sizeof(short) * DATA_SIZE - BITS8;
+    header.format = WAVE;
+    header.subChunk1Id = FMT;
+    header.subChunk1Size = LFORMAT;
+    header.audioFormat = PCM;
+    header.numChannels = CHANNELS;
+    header.sampleRate = SRATE;
+    header.byteRate = BRATE;
+    header.blockAlign = BALIGN;
+    header.bitsPerSample = BPS;
+    header.subChunk2Id = DATA;
+    header.subChunk2Size = (DATA_SIZE * CHANNELS * BPS) / 8;
+
+
+    for(int i = 0; i < DATA_SIZE ; i++){
+        data[i] = 16000 * (sin(6.28 * i));
+        printf("%d\n", data[i]);
+    }
+
+    FILE * f = fopen("subor2.wav", "w");
+
+    if(f != NULL){
+        size_t s;
+
+        s = fwrite(&header, sizeof(header), 1, f);
+        printf("size is: %d", s);
+
+        s = fwrite(data, sizeof(short), DATA_SIZE, f);
+        printf("size is: %d", s);
+
+        fclose(f);
+    }
+
+    free(data);
     return 0;
 }
--- main.h
+++ main.h
@@ -1,7 +1,3 @@
-//
-// Created by halus on 22-Jan-17.
-//
-
 #ifndef PCM_TO_WAV_MAIN_H
 #define PCM_TO_WAV_MAIN_H
 
@@ -22,5 +18,18 @@ typedef struct t_header {
     uint32_t subChunk2Id;
     uint32_t subChunk2Size;
 };
+
+#define RIFF        0x46464952
+#define WAVE        0x45564157
+#define FMT         0x20746d66
+#define LFORMAT     16
+#define PCM         0x0001
+#define CHANNELS    0x0001
+#define SRATE       44100
+#define BPS         16
+#define BRATE       (SRATE * CHANNELS * BPS) / 8
+#define BALIGN      (CHANNELS * BPS) / 8
+#define DATA        0x61746164
+#define BITS8       8
 
 #endif //PCM_TO_WAV_MAIN_H
