ffcefcd8084ddcda668b972d4d87518db4040db2
Juraj Haluska <Juraj Haluska>
2017-01-24 21:50:21 +0200

    mxing wrks

--- main.c
+++ main.c
@@ -5,51 +5,52 @@
 #include "mixer.h"
 
 #define SRATE       44100
-#define DATA_SIZE   SRATE * 10  // 10 seconds
+#define DATA_SIZE   SRATE * 10 // 10 seconds
 
 int main() {
 
     int16_t * data1 = (int16_t *) malloc(DATA_SIZE * sizeof(int16_t));
-
-    for(int i = 0; i < DATA_SIZE ; i++){
-        data1[i] = 32000 * sin( (2 * M_PI * 880  * i) / SRATE);
-    }
-
     int16_t * data2 = (int16_t *) malloc(DATA_SIZE * sizeof(int16_t));
-
-    for(int i = 0; i < DATA_SIZE ; i++){
-        data2[i] = 32000 * sin( (2 * M_PI * 440  * i) / SRATE);
-    }
-/*
     int16_t * data3 = (int16_t *) malloc(DATA_SIZE * sizeof(int16_t));
-
-    for(int i = 0; i < DATA_SIZE ; i++){
-        data3[i] = 32000 * sin( (2 * M_PI * 220  * i) / SRATE);
-    }
-
     int16_t * data4 = (int16_t *) malloc(DATA_SIZE * sizeof(int16_t));
-
-    for(int i = 0; i < DATA_SIZE ; i++){
-        data4[i] = 32000 * sin( (2 * M_PI * 1600  * i) / SRATE);
-    }
-
     int16_t * data5 = (int16_t *) malloc(DATA_SIZE * sizeof(int16_t));
 
     for(int i = 0; i < DATA_SIZE ; i++){
-        data5[i] = 32000 * sin( (2 * M_PI * 2400  * i) / SRATE);
+        data1[i] = 32000 * sin( (2 * M_PI * 880  * i) / SRATE);
+        data2[i] = 32000 * sin( (2 * M_PI * 500  * i) / SRATE);
+        data3[i] = 32000 * sin( (2 * M_PI * 1000  * i) / SRATE);
+        data4[i] = 32000 * sin( (2 * M_PI * 50  * i) / SRATE);
+        data5[i] = 32000 * sin( (2 * M_PI * 1600  * i) / SRATE);
     }
-*/
-    int16_t ** map = malloc(sizeof(int16_t *) * 2);
-    map[0] = data1;
-    map[1] = data2;
-    //map[2] = data3;
-    //map[3] = data4;
-    //map[4] = data5;
 
+
+    double * sum = calloc(sizeof(double), DATA_SIZE);
     int16_t * out = (int16_t *) malloc(sizeof(int16_t) * DATA_SIZE);
 
-    mix(map, 2, DATA_SIZE, out);
+    for(int i = 0; i < DATA_SIZE; i++){
+        sum[i] = (float)data1[i] / 30000.0;
+        //printf("i:%d/%f\n", i, sum[i]);
+    }
+    for(int i = 0; i < DATA_SIZE; i++){
+        sum[i] += (float)data2[i] / 30000.0;
+        //printf("i:%d/%f\n", i, sum[i]);
+    }
+    for(int i = 0; i < DATA_SIZE; i++){
+        sum[i] += (float)data3[i] / 30000.0;
+        //printf("i:%d/%f\n", i, sum[i]);
+    }
+    for(int i = 0; i < DATA_SIZE; i++){
+        sum[i] += (float)data4[i] / 30000.0;
+        //printf("i:%d/%f\n", i, sum[i]);
+    }
+    for(int i = 0; i < DATA_SIZE; i++){
+        sum[i] += (float)data5[i] / 30000.0;
+        //printf("i:%d/%f\n", i, sum[i]);
+    }
 
+    for(int i = 0; i < DATA_SIZE; i++){
+        out[i] = (int)(sum[i] * 5000);
+    }
 
     wavfile wave = wavOpen("mixed.wav", DATA_SIZE, SRATE);
 
@@ -59,14 +60,10 @@ int main() {
 
     wavClose(wave);
 
-
+    free(sum);
     free(out);
-    free(map);
     free(data1);
     free(data2);
-    //free(data3);
-    //free(data4);
-    //free(data5);
 
     return 0;
 }
